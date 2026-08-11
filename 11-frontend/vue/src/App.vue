04c730ecc1f6200fe13c6f80bbb8e69741772e13
Rodrigo Branas <rodrigo.branas@gmail.com>
2026-08-11 13:20:30 -0300

    frontend

--- 11-frontend/vue/src/App.vue
+++ 11-frontend/vue/src/App.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+
+  let step = ref(1);
+
+  function next () {
+    step.value++;
+  }
 </script>
 
 <template>
-  <h1>branas.io</h1>
+  <div>
+    <div>
+      <span class="span-step">{{ step }}</span>
+    </div>
+    <div>
+      <button class="button-next" @click="next()">Próximo</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- 11-frontend/vue/src/components/__tests__/App.spec.ts
+++ 11-frontend/vue/src/components/__tests__/App.spec.ts
@@ -3,7 +3,9 @@ import { test, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 import App from "../../App.vue";
 
-test("Deve criar uma conta", () => {
+test("Deve criar uma conta", async () => {
   const wrapper = mount(App, {});
-  expect(wrapper.text()).toContain("branas.io");
+  expect(wrapper.get(".span-step").text()).toBe("1");
+  await wrapper.get(".button-next").trigger("click");
+  expect(wrapper.get(".span-step").text()).toBe("2");
 });
