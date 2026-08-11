79ba371d871fc345b4722a3ee84d7fe17376e50c
Rodrigo Branas <rodrigo.branas@gmail.com>
2026-08-11 11:20:33 -0300

    docker

--- 12-docker/main.ts
+++ 12-docker/main.ts
@@ -1,12 +1,15 @@
 import express, { type Request, type Response } from "express";
 import fs from "fs/promises";
 import path from "path";
+import pgp from "pg-promise";
 
 const logFile = path.join(import.meta.dirname, "logs/server.log");
 
 async function main () {
     const app = express();
     app.use(express.json());
+    const databaseConnection = pgp()("postgres://postgres:123456@db:5432/app");
+    await databaseConnection.query("insert into app.account (account_id, name, email, document, password) values ($1, $2, $3, $4, $5)", [crypto.randomUUID(), "John Doe", "john.doe@gmail.com", "111.111.111-11", "asdQWE123"]);
     console.log(process.argv);
     console.log(process.env.PORT);
     console.log(new Date(), "main");
@@ -16,6 +19,14 @@ async function main () {
         process.stdout.write(logEntry);
         fs.appendFile(logFile, logEntry);
         res.end();
+    });
+
+    app.get("/accounts", async (req: Request, res: Response) => {
+        const logEntry = `${new Date()} /accounts\n`;
+        process.stdout.write(logEntry);
+        fs.appendFile(logFile, logEntry);
+        const accountsData = await databaseConnection.query("select * from app.account", []);
+        res.json(accountsData);
     });
 
     app.listen(process.env.PORT);
