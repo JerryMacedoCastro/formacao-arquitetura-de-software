c5e3f386e5d47905613f437857ace11bfe8996b4
Rodrigo Branas <rodrigo.branas@gmail.com>
2026-08-11 15:47:07 -0300

    frontend

--- 11-frontend/frontend/vue/src/App.vue
+++ 11-frontend/frontend/vue/src/App.vue
@@ -1,117 +1,54 @@
 <script setup lang="ts">
   import { inject, ref } from 'vue';
   import type AccountGateway from './gateways/AccountGateway';
-
-  let step = ref(1);
-  let name = ref("");
-  let email = ref("");
-  let document = ref("");
-  let password = ref("");
-  let confirmPassword = ref("");
-  let error = ref("");
-  let success = ref("");
+import SignupForm from './entities/SignupForm';
 
   const accountGateway = inject("accountGateway") as AccountGateway;
 
-  function next () {
-    error.value = "";
-    if (!name.value) {
-      error.value = "Preencha o nome";
-      return;
-    }
-    if (!email.value) {
-      error.value = "Preencha o email";
-      return;
-    }
-    if (!document.value) {
-      error.value = "Preencha o documento";
-      return;
-    }
-    step.value++;
-  }
+  const form = ref(new SignupForm());
 
-  function previous () {
-    step.value--;
-  }
+  // const outputSignup = await accountGateway.signup(input);
+  //   if (outputSignup.accountId) {
+  //     success.value = "Conta criada com sucesso";
+  //   } else {
+  //     error.value = "Falha ao tentar criar a conta";
+  //   }
 
-  async function confirm () {
-    error.value = "";
-    if (!password.value) {
-      error.value = "Preencha a senha";
-      return;
-    }
-    if (!confirmPassword.value) {
-      error.value = "Preencha a confirmação da senha";
-      return;
-    }
-    if (password.value !== confirmPassword.value) {
-      error.value = "A senha e a confirmação da senha devem ser iguais";
-      return;
-    }
-    const input = {
-      name: name.value,
-      email: email.value,
-      document: document.value,
-      password: password.value
-    }
-    const outputSignup = await accountGateway.signup(input);
-    if (outputSignup.accountId) {
-      success.value = "Conta criada com sucesso";
-    } else {
-      error.value = "Falha ao tentar criar a conta";
-    }
-  }
 
-  function getProgress () {
-    let progress = 0;
-    if (name.value) {
-      progress += 25;
-    }
-    if (email.value) {
-      progress += 25;
-    }
-    if (document.value) {
-      progress += 25;
-    }
-    if (password.value && confirmPassword.value && password.value === confirmPassword.value) {
-      progress += 25;
-    }
-    return progress;
-  }
 </script>
 
 <template>
   <div>
     <div>
-      <span class="span-step">{{ step }}</span>
+      <span class="span-step">{{ form.step }}</span>
       <br/>
-      <span class="span-progress">{{ getProgress() }}%</span>
+      <span class="span-progress">{{ form.getProgress() }}%</span>
     </div>
-    <div v-if="step === 1">
+    <div v-if="form.step === 1">
       <div>
-        <input type="text" class="input-name" v-model="name" placeholder="Nome"/>
+        <input type="text" class="input-name" v-model="form.name" placeholder="Nome"/>
       </div>
       <div>
-        <input type="text" class="input-email" v-model="email" placeholder="Email"/>
+        <input type="text" class="input-email" v-model="form.email" placeholder="Email"/>
       </div>
       <div>
-        <input type="text" class="input-document" v-model="document" placeholder="Documento"/>
+        <input type="text" class="input-document" v-model="form.document" placeholder="Documento"/>
       </div>
     </div>
-    <div v-if="step === 2">
+    <div v-if="form.step === 2">
       <div>
-        <input type="text" class="input-password" v-model="password" placeholder="Senha"/>
+        <input type="text" class="input-password" v-model="form.password" placeholder="Senha"/>
       </div>
       <div>
-        <input type="text" class="input-confirm-password" v-model="confirmPassword" placeholder="Confirmação da Senha"/>
+        <input type="text" class="input-confirm-password" v-model="form.confirmPassword" placeholder="Confirmação da Senha"/>
       </div>
     </div>
     <div>
-      <button v-if="step === 1" class="button-next" @click="next()">Continuar</button>
-      <button v-if="step === 2" class="button-previous" @click="previous()">Voltar</button>
-      <button v-if="step === 2" class="button-confirm" @click="confirm()">Confirmar</button>
-      <span class="span-error">{{ error }}</span>
-      <span class="span-success">{{ success }}</span>
+      <button v-if="form.step === 1" class="button-next" @click="form.next()">Continuar</button>
+      <button v-if="form.step === 2" class="button-previous" @click="form.previous()">Voltar</button>
+      <button v-if="form.step === 2" class="button-confirm" @click="form.confirm()">Confirmar</button>
+      <span class="span-error">{{ form.error }}</span>
+      <span class="span-success">{{ form.success }}</span>
     </div>
   </div>
 </template>
---
+++ 11-frontend/frontend/vue/src/entities/SignupForm.ts
@@ -0,0 +1,70 @@
+export default class SignupForm {
+    step = 1;
+    name = "";
+    email = "";
+    document = "";
+    password = "";
+    confirmPassword = "";
+    success = "";
+    error = "";
+
+    next () {
+        this.error = "";
+        if (!this.name) {
+            this.error = "Preencha o nome";
+            return;
+        }
+        if (!this.email) {
+            this.error = "Preencha o email";
+            return;
+        }
+        if (!this.document) {
+            this.error = "Preencha o documento";
+            return;
+        }
+        this.step++;
+    }
+
+    previous () {
+        this.step--;
+    }
+
+    async confirm () {
+        this.error = "";
+        if (!this.password) {
+            this.error = "Preencha a senha";
+            return;
+        }
+        if (!this.confirmPassword) {
+            this.error = "Preencha a confirmação da senha";
+            return;
+        }
+        if (this.password !== this.confirmPassword) {
+            this.error = "A senha e a confirmação da senha devem ser iguais";
+            return;
+        }
+        const input = {
+            name: this.name,
+            email: this.email,
+            document: this.document,
+            password: this.password
+        }
+    }
+
+    getProgress () {
+        let progress = 0;
+        if (this.name) {
+            progress += 25;
+        }
+        if (this.email) {
+            progress += 25;
+        }
+        if (this.document) {
+            progress += 25;
+        }
+        if (this.password && this.confirmPassword && this.password === this.confirmPassword) {
+        progress += 25;
+        }
+        return progress;
+    }
+}
