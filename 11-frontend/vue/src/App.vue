9eb17c17f6338627b45bde9126e7c561f34053ff
Rodrigo Branas <rodrigo.branas@gmail.com>
2026-08-11 13:25:23 -0300

    frontend

--- 11-frontend/vue/src/App.vue
+++ 11-frontend/vue/src/App.vue
@@ -2,9 +2,18 @@
   import { ref } from 'vue';
 
   let step = ref(1);
+  let name = ref("");
 
   function next () {
     step.value++;
+  }
+
+  function getProgress () {
+    let progress = 0;
+    if (name.value) {
+      progress += 25;
+    }
+    return progress;
   }
 </script>
 
@@ -12,6 +21,12 @@
   <div>
     <div>
       <span class="span-step">{{ step }}</span>
+      <span class="span-progress">{{ getProgress() }}%</span>
+    </div>
+    <div>
+      <div>
+        <input type="text" class="input-name" v-model="name" placeholder="Nome"/>
+      </div>
     </div>
     <div>
       <button class="button-next" @click="next()">Próximo</button>
--- 11-frontend/vue/src/components/__tests__/App.spec.ts
+++ 11-frontend/vue/src/components/__tests__/App.spec.ts
@@ -6,6 +6,9 @@ import App from "../../App.vue";
 test("Deve criar uma conta", async () => {
   const wrapper = mount(App, {});
   expect(wrapper.get(".span-step").text()).toBe("1");
+  expect(wrapper.get(".span-progress").text()).toBe("0%");
+  await wrapper.get(".input-name").setValue("John Joe");
+  expect(wrapper.get(".span-progress").text()).toBe("25%");
   await wrapper.get(".button-next").trigger("click");
   expect(wrapper.get(".span-step").text()).toBe("2");
 });
